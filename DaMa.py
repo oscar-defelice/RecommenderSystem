bedc7839274c9cdeaffd66a2406dd8f277893707
Oscar de Felice <oscar.defelice@linkalab.it>
2020-01-14 10:41:34 +0100

    DaMa module created.

---
+++ DaMa.py
@@ -0,0 +1,31 @@
+###
+### DaMa.py
+###
+### Created by Oscar de Felice on 14/01/2020.
+### Copyright © 2020 Oscar de Felice.
+###
+### This program is free software: you can redistribute it and/or modify
+### it under the terms of the GNU General Public License as published by
+### the Free Software Foundation, either version 3 of the License, or
+### (at your option) any later version.
+###
+### This program is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public License for more details.
+###
+### You should have received a copy of the GNU General Public License
+### along with this program. If not, see <http://www.gnu.org/licenses/>.
+###
+########################################################################
+###
+### DaMa.py
+### This module contains objects to convert datasets into a matrix of
+### features.
+###
+
+"""
+DAta to MAtrix module.
+
+This module contains objects to convert datasets into a matrix of features.
+"""
